edfb7ac878e5f429f7f01a4e3367a63c21a25167
rafdul <rafalduluk@gmail.com>
2021-05-25 21:55:02 +0200

    Add grid to view of single offer

--- src/components/views/Offer/Offer.js
+++ src/components/views/Offer/Offer.js
@@ -29,44 +29,36 @@ class Component extends React.Component {
     return(
       <div className={clsx(className, styles.root)}>
         <h2 className={styles.title}>About offer: {oneOffer.name}</h2>
-        <Grid container justify="space-between">
-          <Grid item xs={12} md={6}>
-            <Paper className={styles.box}>
-              <Typography variant="body1" component="div" className={styles.box__item}><span className={styles.box__text}>Name:</span> {oneOffer.name}</Typography>
-              <Typography variant="body1" component="div" className={styles.box__item}><span className={styles.box__text}>City:</span>  {oneOffer.city}</Typography>
-              <Typography variant="body1" component="div" className={styles.box__item}><span className={styles.box__text}>Description:</span>  {oneOffer.description}</Typography>
-              <Typography variant="body1" component="div" className={styles.box__item}><span className={styles.box__text}>Price:</span>  {oneOffer.price}</Typography>
-            </Paper>
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <Paper className={styles.box}>
-              <Typography variant="body1" component="div" className={styles.box__item}><span className={styles.box__text}>Bedrooms:</span> {oneOffer.bedrooms}</Typography>
-              <Typography variant="body1" component="div" className={styles.box__item}><span className={styles.box__text}>Kitchen:</span>  {oneOffer.kitchen}</Typography>
-              <Typography variant="body1" component="div" className={styles.box__item}><span className={styles.box__text}>Balcony:</span>  {oneOffer.balcony}</Typography>
-              <Typography variant="body1" component="div" className={styles.box__item}><span className={styles.box__text}>Swimpool:</span>  {oneOffer.swimpool}</Typography>
-            </Paper>
-          </Grid>
-          <Grid item xs={12}>
-            <div className={styles.imageContainer}>
-              {oneOffer.image && oneOffer.image.map(item => (
-                <Paper key={oneOffer.image.indexOf(item)} className={styles.image}>
-                  <CardMedia
-                    className={styles.image__item}
-                    component="img"
-                    image={oneOffer.image === undefined ? null : item}
-                    title={`${oneOffer.name}_${oneOffer.image.indexOf(item)}`}
-                  />
-                </Paper>
-              ))}
-            </div>
-          </Grid>
-          <Grid item xs={12}>
-            <Paper className={styles.box}>
-              <Typography variant="caption" component="div" className={styles.box__item}><span className={styles.box__text}>Location lat:</span> {oneOffer.location === undefined ? 0 : oneOffer.location.lat}</Typography>
-              <Typography variant="caption" component="div" className={styles.box__item}><span className={styles.box__text}>Location lng:</span> {oneOffer.location === undefined ? 0 : oneOffer.location.lng}</Typography>
-              <Typography variant="caption" component="div" className={styles.box__item}><span className={styles.box__text}>Map link:</span> {oneOffer.map}</Typography>
-            </Paper>
-          </Grid>
+        <Grid container justify="space-between" className={styles.grid}>
+          <Paper className={styles.box + ' ' + styles.grid__item1}>
+            <Typography variant="body1" component="div" className={styles.box__item}><span className={styles.box__text}>Name:</span> {oneOffer.name}</Typography>
+            <Typography variant="body1" component="div" className={styles.box__item}><span className={styles.box__text}>City:</span>  {oneOffer.city}</Typography>
+            <Typography variant="body1" component="div" className={styles.box__item}><span className={styles.box__text}>Description:</span>  {oneOffer.description}</Typography>
+            <Typography variant="body1" component="div" className={styles.box__item}><span className={styles.box__text}>Price:</span>  {oneOffer.price}</Typography>
+          </Paper>
+          <Paper className={styles.box + ' ' + styles.grid__item2}>
+            <Typography variant="body1" component="div" className={styles.box__item}><span className={styles.box__text}>Category:</span> {oneOffer.category}</Typography>
+            <Typography variant="body1" component="div" className={styles.box__item}><span className={styles.box__text}>Bedrooms:</span> {oneOffer.bedrooms}</Typography>
+            <Typography variant="body1" component="div" className={styles.box__item}><span className={styles.box__text}>Kitchen:</span>  {oneOffer.kitchen}</Typography>
+            <Typography variant="body1" component="div" className={styles.box__item}><span className={styles.box__text}>Balcony:</span>  {oneOffer.balcony}</Typography>
+            <Typography variant="body1" component="div" className={styles.box__item}><span className={styles.box__text}>Swimpool:</span>  {oneOffer.swimpool}</Typography>
+          </Paper>
+          <Paper className={styles.box + ' ' + styles.grid__item3}>
+            <Typography variant="caption" component="div" className={styles.box__item}><span className={styles.box__text}>Location lat:</span> {oneOffer.location === undefined ? 0 : oneOffer.location.lat}</Typography>
+            <Typography variant="caption" component="div" className={styles.box__item}><span className={styles.box__text}>Location lng:</span> {oneOffer.location === undefined ? 0 : oneOffer.location.lng}</Typography>
+            <Typography variant="caption" component="div" className={styles.box__item}><span className={styles.box__text}>Google map iframe:</span> {oneOffer.map}</Typography>
+          </Paper>
+          <div className={styles.imageContainer + ' ' + styles.grid__item4}>
+            {oneOffer.image && oneOffer.image.map(item => (
+              <div key={oneOffer.image.indexOf(item)} className={styles.image}>
+                <img
+                  className={styles.image__item}
+                  src={oneOffer.image === undefined ? null : item}
+                  alt={`${oneOffer.name}_${oneOffer.image.indexOf(item)}`}
+                />
+              </div>
+            ))}
+          </div>
         </Grid>
       </div>
     );
